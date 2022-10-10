b5f2521e48617a0b57c44f06a6e79ef85f329ff3
ss mm <108014479+ahgedashi@users.noreply.github.com>
2022-10-10 17:44:35 +0900

    mecha ganbatta yo

--- App.jsx
+++ App.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
-import AppBar from './src/components/AppBar';
-import CircleButton from './src/components/CircleButton';
-import MemoList from './src/components/MemoList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-
-    </View>
+    <MemoDetailScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  }, 
-});
